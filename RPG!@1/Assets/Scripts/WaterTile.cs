8a0ad55e2b4eb86cc01df19b8e29b5c1224249aa
lip ables <phillipables1@gmail.com>
2018-04-07 13:55:03 -0400

    Sprite array and Sprite preview object, #if UNITY_EDITOR then MenuItem in the assets create tiles watertile, CreateWaterTile()-> create path if path is found then create asset

--- RPG!@1/Assets/Scripts/WaterTile.cs
+++ RPG!@1/Assets/Scripts/WaterTile.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditor;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class WaterTile : SpriteTileMode {
+public class WaterTile : Tile {
+    [SerializeField]
+    private Sprite[] waterSprites;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private Sprite preview;
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/WaterTile"]
+    public static void CreateWaterTile()
+    {
+        //create a path inside our project
+        string path = EditorUtility.SaveFilePanelInProject("Save Watertile", "New Watertile", "asset", "Save watertile", "Assets");
+        if(path == "")
+        {
+            return;
+        }
+        //if path exist we create the asset in our asset folder
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<WaterTile>(), path)
+    }
+#endif
 }
