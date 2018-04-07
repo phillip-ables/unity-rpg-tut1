d415074aa469b348d12889cda2a54643346a8932
lip ables <phillipables1@gmail.com>
2018-04-07 15:35:40 -0400

    RefreshTile override check all the current position neighbors for y and x of -1 to 1 create a Vector3Int for nPos, GetTileData set tileData sprite to water Sprite index 0;

--- RPG!@1/Assets/Scripts/WaterTile.cs
+++ RPG!@1/Assets/Scripts/WaterTile.cs
@@ -9,6 +9,27 @@ public class WaterTile : Tile {
     [SerializeField]
     private Sprite preview;
 
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {//make sure we refresh all the tiles
+        for(int y = -1; y <= 1; y++)//check all your neighbors
+        {
+            for(int x = -1; x <= 1; x++)
+            {
+                Vector3Int nPos = new Vector3Int(position.x + x, position.y + y, position.z);
+
+            }
+
+        }
+    }
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {//change tile data for us when you refresh a tile
+        tileData.sprite = waterSprites[0];
+    }
+    
+
+
+
 #if UNITY_EDITOR
     [MenuItem("Assets/Create/Tiles/WaterTile")]
     public static void CreateWaterTile()
