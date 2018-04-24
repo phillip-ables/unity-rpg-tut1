c715644d5f6ba24224622f847155081f99fffa6d
lip ables <phillipables1@gmail.com>
2018-04-24 15:03:45 -0400

    copy line renderer script into mesh renderer script, require mesh, replace renderer with MeshFilter().mesh, and rename render arc method to make mesh method

--- RPG!@1/Assets/LaunchArcMesh.cs
+++ RPG!@1/Assets/LaunchArcMesh.cs
@@ -2,15 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaunchArcMesh : MonoBehaviour {
+[RequireComponent(typeof(MeshFilter))]
+public class LaunchArcMesh : MonoBehaviour 
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Mesh mesh;
+    public float meshWidth;
+
+    public float velocity;
+    public float angle;
+    public int resolution;
+
+    float g;
+    float radianAngle;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        g = Mathf.Abs(Physics2D.gravity.y);
+    }
+
+    //if inspector of this class changes, run this code agian;
+    private void OnValidate()
+    {
+        if (mesh != null && Application.isPlaying)
+        {
+            MakeArcMesh();
+        }
+    }
+
+    private void Start()
+    {
+        MakeArcMesh();
+    }
+
+    //populating line renderer with settings
+    void MakeArcMesh()
+    {
+
+    }
+
+    Vector3[] CalculateArray()
+    {
+        Vector3[] arcArray = new Vector3[resolution + 1];
+
+        radianAngle = Mathf.Deg2Rad * angle;
+        float maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / g;
+
+        for (int i = 0; i <= resolution; i++)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = CalculateArcPoint(t, maxDistance);
+        }
+
+        return arcArray;
+    }
+
+    //calculate height and distance of each vertex
+    Vector3 CalculateArcPoint(float t, float maxDistance)
+    {
+        float x = t * maxDistance;
+        float y = x * Mathf.Tan(radianAngle) - ((g * x * x) / (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
+        return new Vector3(x, y);
+    }
 }
+
