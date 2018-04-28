9047ca3fbe1a8a46b79f99b710816bcd1f5342f7
lip ables <phillipables1@gmail.com>
2018-04-28 00:21:34 -0400

    pretty proud, with my own script i made the player move left to right, jump, and face the direction he was moving

--- RPG!@1/Assets/SuperPlatFormBrothers/PlayerMove.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/PlayerMove.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int playerSpeed;
+    public bool facingRight;
+    public int playerJumpPower;
+
+    private float horizontalAxis;
+    private Vector3 playerMove;
+
+    private void Start()
+    {
+        Vector3 playerMove = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 playerMove = transform.position;
+
+        horizontalAxis = Input.GetAxis("Horizontal");
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            facingRight = true;
+            Debug.Log(facingRight);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            facingRight = false;
+            Debug.Log(facingRight);
+        }
+
+        playerMove.x += horizontalAxis * playerSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y < 1)
+            playerMove.y += playerJumpPower;
+
+        if (facingRight)
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        else
+            transform.rotation = Quaternion.Euler(0.0f, 180f, 0.0f);
+        transform.position = playerMove;
+
+    }
+
 }
