0e28ca6cb7b6be5abe0fbd8e44317205ff8d0ad0
lip ables <phillipables1@gmail.com>
2018-04-28 18:10:56 -0400

    RaycastHit2d of position to new vector2 for moveDirection and 0, if that distance is less than public float hitDistance then call flip method, enemy remove from ray cast collision layer

--- RPG!@1/Assets/EnemyMove.cs
+++ RPG!@1/Assets/EnemyMove.cs
@@ -6,10 +6,21 @@ public class EnemyMove : MonoBehaviour {
 
     public int EnemySpeed;
     public int xMoveDirection;
+    public int hitDistance;
 
     private void Update()
     {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0.0f));
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMoveDirection, 0) * EnemySpeed;
+
+        if (hit.distance < hitDistance)
+            Flip();
+
+    }
+
+    void Flip()
+    {
+
     }
 
 }
