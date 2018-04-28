481169798d645244c8c2b433b6acd4fb7ed39d74
lip ables <phillipables1@gmail.com>
2018-04-28 18:34:30 -0400

    playerscore-> public float time left, on update time left minus equals time dot delta time

--- RPG!@1/Assets/SuperPlatFormBrothers/Scripts/PlayerScore.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/PlayerScore.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class PlayerScore : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float timeLeft = 120;
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+    }
+
 }
