9d66b2a67aa594abb4e24ef7ba21a5e86d82700c
lip ables <phillipables1@gmail.com>
2018-04-28 12:22:06 -0400

    on late update float x = player position.x clamped to min and max value of x, same for y, transform gameObject position to these new var

--- RPG!@1/Assets/CameraSystem.cs
+++ RPG!@1/Assets/CameraSystem.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraSystem : MonoBehaviour {
-    private GameObject player;
     public float xMin;
     public float xMax;
     public float yMin;
     public float yMax;
 
+    private GameObject player;
+
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void LateUpdate()
+    {
+        float x = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
     }
 }
