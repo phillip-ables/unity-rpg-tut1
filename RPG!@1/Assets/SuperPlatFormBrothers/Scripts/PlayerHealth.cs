1a52413e37d4c7aff9050a55274a57b522919218
lip ables <phillipables1@gmail.com>
2018-04-28 15:34:11 -0400

    import sceneManager, Die IEnumerator-> cleared, LoadScene , yield return null

--- RPG!@1/Assets/SuperPlatFormBrothers/Scripts/PlayerHealth.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour {
 
@@ -29,8 +30,7 @@ public class PlayerHealth : MonoBehaviour {
 
     IEnumerator Die()
     {
-        Debug.Log("Player Has Fallen");
-        yield return new WaitForSeconds(2);
-        Debug.Log("Player Has Died!!");
+        SceneManager.LoadScene("SuperPlatformer");
+        yield return null;
     }
 }
