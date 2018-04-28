abdd9f91bd106e15a9352392af8f245657b12cba
lip ables <phillipables1@gmail.com>
2018-04-28 00:30:52 -0400

    wiped down to rejoin tutorial, in update call moveplayer function which will hold the controls animations player direction and physics, also create empty jump method for jumping code

--- RPG!@1/Assets/SuperPlatFormBrothers/PlayerMove.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/PlayerMove.cs
@@ -4,44 +4,26 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-    public int playerSpeed;
-    public bool facingRight;
-    public int playerJumpPower;
-
-    private float horizontalAxis;
-    private Vector3 playerMove;
-
-    private void Start()
-    {
-        Vector3 playerMove = transform.position;
-    }
+    public int playerSpeed = 10;
+    public bool facingRight = true;
+    public int playerJumpPower = 1250;
+    public float moveX;
 
     private void Update()
     {
-        Vector3 playerMove = transform.position;
-
-        horizontalAxis = Input.GetAxis("Horizontal");
-        if (Input.GetAxis("Horizontal") > 0)
-        {
-            facingRight = true;
-            Debug.Log(facingRight);
-        }
-        if (Input.GetAxis("Horizontal") < 0)
-        {
-            facingRight = false;
-            Debug.Log(facingRight);
-        }
-
-        playerMove.x += horizontalAxis * playerSpeed * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y < 1)
-            playerMove.y += playerJumpPower;
-
-        if (facingRight)
-            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
-        else
-            transform.rotation = Quaternion.Euler(0.0f, 180f, 0.0f);
-        transform.position = playerMove;
-
+        MovePlayer();
     }
 
+    void MovePlayer()
+    {
+        //CONTROLS
+        //ANIMATIONS
+        //PLAYER DIRECTION
+        //PHYSICS
+    }
+
+    void Jump()
+    {
+        //JUMPING CODE
+    }
 }
