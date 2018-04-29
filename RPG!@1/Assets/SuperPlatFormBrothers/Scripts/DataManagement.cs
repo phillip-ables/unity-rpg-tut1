81e00478817ce05c84dc6354e742cd01732be149
lip ables <phillipables1@gmail.com>
2018-04-29 00:55:51 -0400

    sprite renderer added to finish object

--- RPG!@1/Assets/SuperPlatFormBrothers/Scripts/DataManagement.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/DataManagement.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class DataManagement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
