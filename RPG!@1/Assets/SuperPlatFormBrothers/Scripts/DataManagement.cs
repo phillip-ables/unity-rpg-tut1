658af3793de329042370cb863fa9e01df23e084a
lip ables <phillipables1@gmail.com>
2018-04-29 12:13:31 -0400

    data management-> import System, System IO, System Runtime Serialization Formatters Bianary

--- RPG!@1/Assets/SuperPlatFormBrothers/Scripts/DataManagement.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/DataManagement.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class DataManagement : MonoBehaviour {
 
+    
 
 }
