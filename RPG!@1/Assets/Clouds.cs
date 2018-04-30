8e61456acca2522122255660048f04c9e98f5f4d
lip ables <phillipables1@gmail.com>
2018-04-29 20:14:05 -0400

    cloud script added to clouds object with speed and xDir, on start xDir equals position x, on update xDir minus equals time dot delta time and update transform position

--- RPG!@1/Assets/Clouds.cs
+++ RPG!@1/Assets/Clouds.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Clouds : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+    private float xDir;
+
+    private void Start()
+    {
+        xDir = transform.position.x;
+    }
+
+    private void Update()
+    {
+        xDir -= Time.deltaTime * speed;
+        transform.position = new Vector3(xDir, transform.position.y, transform.position.z);
+    }
+
 }
