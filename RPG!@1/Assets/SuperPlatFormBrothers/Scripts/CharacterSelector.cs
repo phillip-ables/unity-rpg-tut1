ee3b77614f61a660d32710cb3dcc4abec5d6450c
lip ables <phillipables1@gmail.com>
2018-04-30 19:06:37 -0400

    clear script with two empty methods

--- RPG!@1/Assets/SuperPlatFormBrothers/Scripts/CharacterSelector.cs
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/CharacterSelector.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelector : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ChooseCharacter(int characterIndex)
+    {
+
+    }
+
+    public void LoadScene()
+    {
+
+    }
+
 }
