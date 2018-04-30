915403eb7080ad91e39c9c7bc8531e56eafda107
lip ables <phillipables1@gmail.com>
2018-04-29 22:47:25 -0400

    black sprite as a door, and a teleport door script created, empty game object for spawn point set on the underground

---
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/Clouds.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+    public float speed;
+    private float xDir;
+
+    private void Start()
+    {
+        xDir = transform.position.x;
+    }
+
+    private void Update()
+    {
+        xDir -= Time.deltaTime * speed;
+        transform.position = new Vector3(xDir, transform.position.y, transform.position.z);
+    }
+
+}
