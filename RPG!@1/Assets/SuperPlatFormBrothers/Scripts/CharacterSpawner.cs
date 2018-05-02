dd977ec976c4b1ee06b458a1e00bc64a3a322b36
lip ables <phillipables1@gmail.com>
2018-05-02 08:32:45 -0400

    MeatBoy scene for meatboy control tutorial https://www.youtube.com/watch?v=tfcVRz0-sfk

---
+++ RPG!@1/Assets/SuperPlatFormBrothers/Scripts/CharacterSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSpawner : MonoBehaviour {
+
+    public GameObject[] players;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("SelectedCharacter") == 0)
+            Instantiate(players[(0)], Vector2.zero, Quaternion.identity);
+        else
+            Instantiate(players[(1)], Vector2.zero, Quaternion.identity);
+    }
+
+}
