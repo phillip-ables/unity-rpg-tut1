45e02a7014997d8e46b40af4c03c3aa68f742fb3
lip ables <phillipables1@gmail.com>
2018-05-03 16:35:30 -0400

    worm body sprite added, worm script added to empty wormbody object, instantiate wormbody for vertex distance divided by sprite length from last position plus spritelength

--- RPG!@1/Assets/MeatBoyMovement/Scripts/PlayerInput.cs
+++ RPG!@1/Assets/MeatBoyMovement/Scripts/PlayerInput.cs
@@ -4,16 +4,4 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour {
     
-    private bool isOnGround()
-    {
-        float lengthToSearch = 0.1f;
-        float colliderThreshold = 0.001f;
-
-        //this is using the bottom of the sprite might cause error
-        Vector2 linestart = new Vector2(this.transform.position.x, this.transform.position.y - this.renderer.bounds.extends.y - colliderThreshold);
-
-        Vector2 vectorToSeach = new Vector2(this.transform.position.x, linestart.y - lengthToSearch);
-
-    }
-
 }
---
+++ RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Worm : MonoBehaviour {
+
+    public Vector2[] verticies;
+    public GameObject wormSprite;
+    private float vertixDistance = 5.0f;
+    private float spriteLength = 0.18f;
+    private float straightLength;
+    private Vector3 currentPos;
+    
+
+	void Start () {
+        currentPos = transform.position;
+
+        //positions verticies
+
+        for(int i = 0; i < vertixDistance/spriteLength; i++)
+        {
+            Instantiate(wormSprite, currentPos, Quaternion.identity);
+            currentPos.x += spriteLength;
+        }
+    }
+	
+}
