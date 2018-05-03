87659c1b7c98f20eb62b50158f08834a7a357603
lip ables <phillipables1@gmail.com>
2018-05-03 17:32:18 -0400

    iterates through vertix array length, sets change of x per distance divided by sprite length which is what the nested loop iterates over distance divided by sprite length, instantiate then increment the currentposition vector by the change per variables, also vertexies now have the whole lvl snake added

--- RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
+++ RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
@@ -6,27 +6,56 @@ public class Worm : MonoBehaviour {
 
     public Vector2[] verticies;
     public GameObject wormSprite;
-    private float vertixDistance = 5.0f;
+    public float straightLength;
+    public float xChangePer, yChangePer;
+    public int count = 0;
+
     private float spriteLength = 0.18f;
-    private float straightLength;
+    //private float slope;
     private Vector3 currentPos;
     
 
-	void Start () {
+	void Awake () {
         currentPos = transform.position;
 
         //positions verticies
         Vector2[] verticies =
         {
             new Vector2(1.736f, -2.098f),
-            new Vector2(1.736f, 1.93f)
+            new Vector2(1.736f, 0.602f),
+            new Vector2(-3.34f, 0.602f),
+            new Vector2(-3.34f, 2.54f),
+            new Vector2(2.43f, 1.142f),
+            new Vector2(3.88f, -2.7f)
         };
 
-        for(int i = 0; i < vertixDistance/spriteLength; i++)
+        for(int i = 0; i <= verticies.Length; i++) //2
         {
-            Instantiate(wormSprite, currentPos, Quaternion.identity);
-            currentPos.x += spriteLength;
+            //rise over run--> change in y divided by change in x
+            //slope = (verticies[i].y - currentPos.y) / (verticies[i].x - currentPos.x);
+
+            //distance formula
+            straightLength = Mathf.Sqrt(
+                                        ((verticies[i].y - currentPos.y) * (verticies[i].y - currentPos.y))
+                                        + ((verticies[i].x - currentPos.x) * (verticies[i].x - currentPos.x))
+                                        );
+
+
+            xChangePer = (verticies[i].x - currentPos.x) / (straightLength / spriteLength);
+            yChangePer = (verticies[i].y - currentPos.y) / (straightLength / spriteLength);
+
+            for (int j = 0; j < straightLength / spriteLength; j++)
+            {
+                Instantiate(wormSprite, currentPos, Quaternion.identity);
+                currentPos = new Vector3(
+                                        currentPos.x + xChangePer,
+                                        currentPos.y + yChangePer,
+                                        currentPos.z
+                                        );
+                count++;
+            }
         }
+        print(count);
     }
 	
 }
