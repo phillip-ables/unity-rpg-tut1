614d0562ab7d5f0f91a3b4baaf6cbbc7b63ed020
lip ables <phillipables1@gmail.com>
2018-05-02 23:59:35 -0400

    isOnGround-> new vector2 vector to seach from this x position, to linestart.y minus length to search

--- RPG!@1/Assets/MeatBoyMovement/Scripts/PlayerInput.cs
+++ RPG!@1/Assets/MeatBoyMovement/Scripts/PlayerInput.cs
@@ -11,6 +11,9 @@ public class PlayerInput : MonoBehaviour {
 
         //this is using the bottom of the sprite might cause error
         Vector2 linestart = new Vector2(this.transform.position.x, this.transform.position.y - this.renderer.bounds.extends.y - colliderThreshold);
+
+        Vector2 vectorToSeach = new Vector2(this.transform.position.x, linestart.y - lengthToSearch);
+
     }
 
 }
