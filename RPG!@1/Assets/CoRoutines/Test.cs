ccb649b946436fb6b0ad1da62a37b6f28edd7ee2
lip ables <phillipables1@gmail.com>
2018-05-04 17:17:13 -0400

    test-> start method and IEnumerator called PrintMessages that take a string array and a float

--- RPG!@1/Assets/CoRoutines/Test.cs
+++ RPG!@1/Assets/CoRoutines/Test.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Start()
+    {
+        
+    }
+
+    IEnumerator PrintMessages(string[] messages, float delay)
+    {
+
+    }
 }
