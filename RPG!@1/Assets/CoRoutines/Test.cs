e268fe10dd9777f33f2378577e71c60c6770c096
lip ables <phillipables1@gmail.com>
2018-05-04 19:03:51 -0400

    Move IEnumerator that takes a vector3 destination and a float for soeed, while position is not destination MoveTowards destination at speed times delta time and yield null to wait one frame each move

--- RPG!@1/Assets/CoRoutines/Test.cs
+++ RPG!@1/Assets/CoRoutines/Test.cs
@@ -8,9 +8,18 @@ public class Test : MonoBehaviour {
     private void Start()
     {
 
-        string[] messages = { "welcom", "TO", "this", "GAME" };
+        string[] messages = { "welcom", "TO", "this", "AMAZING", "game" };
         StartCoroutine(PrintMessages(messages, .5f));
 
+    }
+
+    IEnumerator Move(Vector3 destination, float speed)
+    {
+        while(transform.position != destination)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            yield return null;  // wait one frame
+        }
     }
 
     IEnumerator PrintMessages(string[] messages, float delay)
