7aafd4e1860d42cb8abc406728bc54720fcf9930
lip ables <phillipables1@gmail.com>
2018-05-04 20:39:09 -0400

    on start just starts a coroutine everything else including the hard coded verticies are in the IEnumerator method MakeWorm

--- RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
+++ RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
@@ -6,20 +6,26 @@ public class Worm : MonoBehaviour {
 
     public Vector2[] verticies;
     public GameObject wormSprite;
-    public float straightLength;
-    public float xChangePer, yChangePer;
-    public int count = 0;
-    public float iterRate;
+    public float slowDistance;
+
 
     private float spriteLength = 0.18f;
-    //private float slope;
+    private float straightLength;
+    private float xChangePer, yChangePer;
+    private float delay;
     private Vector3 currentPos;
     
 
-	void Awake () {
+	void Start () {
+        StartCoroutine(MakeSnake());
+    }
+
+    IEnumerator MakeSnake()
+    {
         currentPos = transform.position;
 
         //positions verticies
+        //this should be turned into a public transform of waypoint gameObjects
         Vector2[] verticies =
         {
             new Vector2(1.736f, -2.098f),
@@ -29,12 +35,8 @@ public class Worm : MonoBehaviour {
             new Vector2(2.43f, 1.142f),
             new Vector2(3.88f, -2.7f)
         };
-
-        for(int i = 0; i <= verticies.Length; i++) //2
+        for (int i = 0; i <= verticies.Length; i++) //2
         {
-            //rise over run--> change in y divided by change in x
-            //slope = (verticies[i].y - currentPos.y) / (verticies[i].x - currentPos.x);
-
             //distance formula
             straightLength = Mathf.Sqrt(
                                         ((verticies[i].y - currentPos.y) * (verticies[i].y - currentPos.y))
@@ -46,22 +48,16 @@ public class Worm : MonoBehaviour {
             yChangePer = (verticies[i].y - currentPos.y) / (straightLength / spriteLength);
 
             for (int j = 0; j < straightLength / spriteLength; j++)
-            {                
+            {
                 
-                StartCoroutine(InstaWorm());
+                Instantiate(wormSprite, currentPos, Quaternion.identity);
+                currentPos = new Vector3(
+                                currentPos.x + xChangePer,
+                                currentPos.y + yChangePer,
+                                currentPos.z
+                                );
+                yield return new WaitForSeconds(delay);
             }
         }
-        print(count);
-    }
-
-    IEnumerator InstaWorm()
-    {
-        Instantiate(wormSprite, currentPos, Quaternion.identity);
-        currentPos = new Vector3(
-                        currentPos.x + xChangePer,
-                        currentPos.y + yChangePer,
-                        currentPos.z
-                        );
-        yield return new WaitForSeconds(iterRate);
     }
 }
