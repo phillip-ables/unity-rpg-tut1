230491dc9d02642861ea5f4189c1d35b4472ba2d
lip ables <phillipables1@gmail.com>
2018-05-05 09:26:11 -0400

    Vector2 array switched to transform array with empty objects set to the vector2 positions  ---VERY IMPORTANT

--- RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
+++ RPG!@1/Assets/MeatBoyMovement/Scripts/Worm.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Worm : MonoBehaviour {
 
-    public Vector2[] verticies;
+    public Transform[] verticies;
     public GameObject wormSprite;
     public GameObject wormHead;
     public float slowDistance;
@@ -34,34 +34,23 @@ public class Worm : MonoBehaviour {
     {
         currentPos = transform.position;
         GameObject head = Instantiate(wormHead, currentPos, Quaternion.identity);
-
-
-        //positions verticies
-        //this should be turned into a public transform of waypoint gameObjects
-        Vector2[] verticies =
-        {
-            new Vector2(1.736f, -2.098f),
-            new Vector2(1.736f, 0.602f),
-            new Vector2(-3.34f, 0.602f),
-            new Vector2(-3.34f, 2.54f),
-            new Vector2(2.43f, 1.142f),
-            new Vector2(3.88f, -2.7f)
-        };
+        
         for (int i = 0; i <= verticies.Length; i++) //2
         {
             //distance formula
-            straightLength = Mathf.Sqrt(((verticies[i].y - currentPos.y) * (verticies[i].y - currentPos.y))
-                                        + ((verticies[i].x - currentPos.x) * (verticies[i].x - currentPos.x)));
-            xChangePer = (verticies[i].x - currentPos.x) / (straightLength / spriteLength);
-            yChangePer = (verticies[i].y - currentPos.y) / (straightLength / spriteLength);
-            
+            straightLength = Mathf.Sqrt(((verticies[i].position.y - currentPos.y) * (verticies[i].position.y - currentPos.y))
+                                        + ((verticies[i].position.x - currentPos.x) * (verticies[i].position.x - currentPos.x)));
+            xChangePer = (verticies[i].position.x - currentPos.x) / (straightLength / spriteLength);
+            yChangePer = (verticies[i].position.y - currentPos.y) / (straightLength / spriteLength);
+
 
             //look at next vertex!!
+            head.transform.LookAt(verticies[i].position);
             for (int j = 0; j < straightLength / spriteLength; j++)
             {
                 //need a slow down effect
-                if (Mathf.Abs((verticies[i].x - currentPos.x)) < slowDistance
-                        && Mathf.Abs((verticies[i].y - currentPos.y)) < slowDistance)
+                if (Mathf.Abs((verticies[i].position.x - currentPos.x)) < slowDistance
+                        && Mathf.Abs((verticies[i].position.y - currentPos.y)) < slowDistance)
                 {
                     delay *= wormAcc;
                     print("SlowDown "+delay);
