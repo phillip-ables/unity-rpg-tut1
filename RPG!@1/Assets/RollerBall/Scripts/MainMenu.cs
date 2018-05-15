919128c38d5f56b950005f4ed9161149cc8c57cf
lip ables <phillipables1@gmail.com>
2018-05-15 16:39:22 -0400

    empty game object for main menu script, reset position, add public game object for button, turn levelbutton into prefab and link to script

--- RPG!@1/Assets/RollerBall/Scripts/MainMenu.cs
+++ RPG!@1/Assets/RollerBall/Scripts/MainMenu.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-    
+    public GameObject levelButtonPrefab;
 }
