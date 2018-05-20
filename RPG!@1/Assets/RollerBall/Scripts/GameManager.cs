ec057f08c25ff2661406222f794420bc37eef132
lip ables <phillipables1@gmail.com>
2018-05-20 15:56:38 -0400

    private instance type of Gamaanager and a public type GameManager get and return instance

--- RPG!@1/Assets/RollerBall/Scripts/GameManager.cs
+++ RPG!@1/Assets/RollerBall/Scripts/GameManager.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    
+
+    private static GameManager instance;
+    public static GameManager Instance{get{ return instance; } }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
 }
