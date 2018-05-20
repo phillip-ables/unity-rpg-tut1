14de040c8790c7b07f3f5a18cd15349a9ed3c0de
lip ables <phillipables1@gmail.com>
2018-05-20 17:04:51 -0400

    if player prefs has no current skin then they have never played and we set the current skin currency and skin avilability to thier initial values

--- RPG!@1/Assets/RollerBall/Scripts/GameManager.cs
+++ RPG!@1/Assets/RollerBall/Scripts/GameManager.cs
@@ -8,13 +8,23 @@ public class GameManager : MonoBehaviour {
     public static GameManager Instance{get{ return instance; } }
 
     public int currentSkinIndex;
-    public uint currecy;
+    public uint currency;
     public uint skinAvalability;
 
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
 
+        if (PlayerPrefs.HasKey("CurrentSkin"))
+        {
 
+        }
+        else
+        {
+            //if weve never played these are our starting stats
+            PlayerPrefs.SetInt("CurrentSkin", 0);
+            PlayerPrefs.SetInt("Currency", 0);
+            PlayerPrefs.SetInt("SkinAvilability", 1);
+        }
     }
 }
