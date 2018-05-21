a168f3bafa0805def0ad97726abccb919ba322be
lip ables <phillipables1@gmail.com>
2018-05-21 18:44:33 -0400

    implement the three event handlers

--- RPG!@1/Assets/RollerBall/Scripts/VirtualJoystick.cs
+++ RPG!@1/Assets/RollerBall/Scripts/VirtualJoystick.cs
@@ -5,5 +5,18 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
-    
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
 }
