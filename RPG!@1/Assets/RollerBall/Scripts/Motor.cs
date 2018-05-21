5b55a514bd1c280b5c4ec0a5d9572cdf9cd3e527
lip ables <phillipables1@gmail.com>
2018-05-21 17:01:38 -0400

    on start get reference to main camera transform, vector3 of cam transform direction to dir, set z explicitly to 0 then times it by our original dir mangnitude normalized

--- RPG!@1/Assets/RollerBall/Scripts/Motor.cs
+++ RPG!@1/Assets/RollerBall/Scripts/Motor.cs
@@ -8,12 +8,15 @@ public class Motor : MonoBehaviour {
     public float terminalRotationSpeed = 25.0f;
 
     private Rigidbody controller;
+    private Transform camTransform;
 
     private void Start()
     {
         controller = GetComponent<Rigidbody>();
         controller.maxAngularVelocity = terminalRotationSpeed;
         controller.drag = drag;
+
+        camTransform = Camera.main.transform;
     }
 
     private void Update()
@@ -26,6 +29,11 @@ public class Motor : MonoBehaviour {
         if (dir.magnitude > 1)
             dir.Normalize();
 
-        controller.AddForce(dir * moveSpeed);
+        // Rotate our direction vector with the camera
+        Vector3 rotatedDir = camTransform.TransformDirection(dir);
+        rotatedDir = new Vector3(rotatedDir.x, 0, rotatedDir.z);
+        rotatedDir = rotatedDir.normalized * dir.magnitude;
+
+        controller.AddForce(rotatedDir * moveSpeed);
     }
 }
