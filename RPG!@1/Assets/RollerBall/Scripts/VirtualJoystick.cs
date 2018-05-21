5186af9b67a34225d84b3c9436437141e5e12b6c
lip ables <phillipables1@gmail.com>
2018-05-21 18:42:58 -0400

    using unity engine ui and event systems, along with mono behaviour we have IDragHandler and IPointerUp and Down Handler

--- RPG!@1/Assets/RollerBall/Scripts/VirtualJoystick.cs
+++ RPG!@1/Assets/RollerBall/Scripts/VirtualJoystick.cs
@@ -1,16 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class VirtualJoystick : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler {
+    
 }
