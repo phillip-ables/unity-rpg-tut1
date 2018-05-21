1b79fa4bbfc035151627cbfa0018fabd5386e2d1
lip ables <phillipables1@gmail.com>
2018-05-21 16:23:23 -0400

    slide camera function that if left bool is true rotates camera ninety if false rotates camera neg ninety, on update if left key down slide camera true, if right false, transform LookAt() our lookat transform

---
+++ RPG!@1/Assets/RollerBall/Scripts/CameraMotor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    public Transform lookAt;
+
+    private Vector3 offset;
+    private float distance = 8.0f;
+    private float yOffset = 3.5f;
+
+    private void Start()
+    {
+        offset = new Vector3(0, yOffset, -1 * distance);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            SlideCamera(true);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            SlideCamera(false);
+
+        transform.position = lookAt.position + offset;
+        transform.LookAt(lookAt);
+    }
+
+    public void SlideCamera(bool left)
+    {
+        if (left)
+            offset = Quaternion.Euler(0, 90, 0) * offset;
+        else
+            offset = Quaternion.Euler(0, -90, 0) * offset;
+    }
+}
