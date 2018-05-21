46d8601961c0b2d668bf1f615d601f3842abb3e2
lip ables <phillipables1@gmail.com>
2018-05-21 15:41:26 -0400

    public floats for moveSpeed drap and terminal rotation speed, private rigidbody contr4oller

--- RPG!@1/Assets/RollerBall/Scripts/Motor.cs
+++ RPG!@1/Assets/RollerBall/Scripts/Motor.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Motor : MonoBehaviour {
+    public float moveSpeed = 5.0f;
+    public float drag = 0.5f;
+    public float terminalRotationSpeed = 25.0f;
+
+    private Rigidbody controller;
 
 
 }
