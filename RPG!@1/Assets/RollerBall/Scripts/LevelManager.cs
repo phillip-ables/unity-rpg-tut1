d85805c50dd1daff38d21af5b755248b97e4ee71
lip ables <phillipables1@gmail.com>
2018-05-21 09:29:22 -0400

    on start set active of pause menu to false

--- RPG!@1/Assets/RollerBall/Scripts/LevelManager.cs
+++ RPG!@1/Assets/RollerBall/Scripts/LevelManager.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
+    public GameObject pauseMenu;
 
+    public void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
 
-
-
-
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
 
     public void ToMenu()
     {
