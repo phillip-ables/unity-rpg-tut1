3e07e9ffa7a46be0c0f8ba0b005d3f70aea9fc72
lip ables <phillipables1@gmail.com>
2018-05-21 19:30:54 -0400

    onDrag listener if inside the virtual joystick then get ratio of position devided by size the convert it to a value we can use

--- RPG!@1/Assets/RollerBall/Scripts/VirtualJoystick.cs
+++ RPG!@1/Assets/RollerBall/Scripts/VirtualJoystick.cs
@@ -16,7 +16,23 @@ public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, I
 
     public void OnDrag(PointerEventData eventData)
     {
-        throw new System.NotImplementedException();
+        Vector2 pos = Vector2.zero;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle
+            (bgImg.rectTransform,
+            eventData.position,
+            eventData.pressEventCamera,
+            out pos))
+        {
+            //if we do manage to click inside our rectangle;]
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.x);
+
+            //once we get that ratio we mutliply it by where that pivot point is
+            float x = (bgImg.rectTransform.pivot.x == 1) ? pos.x * 2 + 1 : pos.x * 2 - 1;
+            float y = (bgImg.rectTransform.pivot.y == 1) ? pos.y * 2 + 1 : pos.y * 2 - 1;
+        }
+
+        //throw new System.NotImplementedException();
     }
 
     public void OnPointerDown(PointerEventData eventData)
