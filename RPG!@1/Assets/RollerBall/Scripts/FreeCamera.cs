1151cad5edd9688674f9592e08f47018ac31c28e
lip ables <phillipables1@gmail.com>
2018-05-21 20:47:09 -0400

    free camera script added to main camera instead of joystick, public objects set, then transform lookat to our player transform called lookat

--- RPG!@1/Assets/RollerBall/Scripts/FreeCamera.cs
+++ RPG!@1/Assets/RollerBall/Scripts/FreeCamera.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FreeCamera : MonoBehaviour {
     public VirtualJoystick cameraJoystick;
 
+    public Transform lookAt;
+
     private float distance = 10.0f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
@@ -19,6 +21,8 @@ public class FreeCamera : MonoBehaviour {
 
     private void LateUpdate()
     {
-        
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        transform.position = lookAt.position + rotation * dir;
     }
 }
