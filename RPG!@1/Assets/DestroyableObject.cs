0e68f34eab0c3ea6b6a7949b6a046d96d404098a
lip ables <phillipables1@gmail.com>
2018-05-23 18:51:50 -0400

    on collision enter if collision impulse magnitude is greater than force required then destroy game object

--- RPG!@1/Assets/DestroyableObject.cs
+++ RPG!@1/Assets/DestroyableObject.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyableObject : MonoBehaviour {
-    public float forceRequired = 3.0f;
+    public float forceRequired = 10.5f;
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.impulse);
+        //7 no run start -- 9-11 with run -- 11-13 with boost
+        if (collision.impulse.magnitude > forceRequired)
+            Destroy(gameObject);
     }
 }
