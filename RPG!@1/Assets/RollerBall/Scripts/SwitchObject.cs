42b52033d4f64512635bb9667c55f624bfe6c55b
lip ables <phillipables1@gmail.com>
2018-05-29 23:52:53 -0400

    switch script gets a public transform for gate, on collision enter destroy ttarget transform

--- RPG!@1/Assets/RollerBall/Scripts/SwitchObject.cs
+++ RPG!@1/Assets/RollerBall/Scripts/SwitchObject.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SwitchObject : MonoBehaviour {
+    public Transform target;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(target);
+    }
 }
