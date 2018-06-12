cb16ba4c2450163915e78003408f211e799e8173
lip ables <phillipables1@gmail.com>
2018-06-12 01:13:28 -0400

    on update loop for childCount of challange object and set instance of child to current child passed through to scrollChallange method that moves the current challanges position towards the object

--- RPG!@1/Assets/EndlessScroller/Scripts/ChallengeScroller.cs
+++ RPG!@1/Assets/EndlessScroller/Scripts/ChallengeScroller.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ChallengeScroller : MonoBehaviour {
+    public float scrollSpeed = 5.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        //Scrolling
+        GameObject currentChild;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            currentChild = transform.GetChild(i).gameObject;
+            ScrollChallenge(currentChild);
+        }
+    }
+
+    void ScrollChallenge (GameObject currentChallenge)
+    {
+        currentChallenge.transform.position -= Vector3.right * (scrollSpeed * Time.deltaTime);
+    }
 }
