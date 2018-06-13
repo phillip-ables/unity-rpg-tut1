73a70da485c15babffbb177cd6260168348c32fb
lip ables <phillipables1@gmail.com>
2018-06-13 00:08:20 -0400

    gameController-> public GameObject for gameoverpanel, gameover method called invokes showgameoverpanel which sets active to true for gameoverpanel

--- RPG!@1/Assets/EndlessScroller/Scripts/GameController.cs
+++ RPG!@1/Assets/EndlessScroller/Scripts/GameController.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
-    
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverPanel;
 
     void GameOver()
     {
+        Invoke("ShowOverPanel", 2.0f);
+    }
 
+    void ShowOverPanel()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
