3c629364a1972aa2fde78e79a6181b291a4da62e
lip ables <phillipables1@gmail.com>
2018-06-15 01:43:17 -0400

    public float backgroundSize, private cameratransform, transform layers array, float viewzone, int left and right index:

--- RPG!@1/Assets/Parrallax/Scripts/ScrollingBackgroundScript.cs
+++ RPG!@1/Assets/Parrallax/Scripts/ScrollingBackgroundScript.cs
@@ -3,5 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScrollingBackgroundScript : MonoBehaviour {
-    
+
+    public float backgroundSize;
+
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private int leftIndex;
+    private int rightIndex;
+
 }
