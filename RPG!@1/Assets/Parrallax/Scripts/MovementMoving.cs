aacb2c48f456a9d9adfdd1e470f75fac01020ad4
lip ables <phillipables1@gmail.com>
2018-06-14 22:08:07 -0400

    movement script

---
+++ RPG!@1/Assets/Parrallax/Scripts/MovementMoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementMoving : MonoBehaviour {
+
+    public float speed;
+    public float horizontal;
+    private Vector3 move;
+
+    private void Start()
+    {
+        Camera.main.transform.Translate(10,0,0);
+
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        move = new Vector3(horizontal, 0);
+        Camera.main.transform.Translate(move);
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = move;
+    }
+
+}
